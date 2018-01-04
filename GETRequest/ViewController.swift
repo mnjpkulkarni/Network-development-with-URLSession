6f1b7660b7edfd4f0a159369571e1327a6e3089c
manojkulkarni <mnjpkulkarni@gmail.com>
2018-01-03 19:37:46 -0500

    Initial Commit

---
+++ GETRequest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GETRequest
+//
+//  Created by Manoj Kulkarni on 1/3/18.
+//  Copyright © 2018 Manoj Kulkarni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
