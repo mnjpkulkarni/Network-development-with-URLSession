640a73b2be103f91a2fad63bebaff34e12320001
manojkulkarni <mnjpkulkarni@gmail.com>
2018-01-03 23:41:52 -0500

    Initial commit

--- GETRequest/ViewController.swift
+++ GETRequest/ViewController.swift
@@ -20,6 +20,51 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        fetchUrl(url: "https://rmala.org/wp-content/uploads/2017/12/punisher.jpg")
+    }
 
+    func fetchUrl(url : String){
+        if let urlToServer = URL.init(string: url){
+            let task = URLSession.shared.downloadTask(with: urlToServer, completionHandler: { (fileUrl, response, error) in
+                
+                if error != nil || fileUrl == nil {
+                    //Error Handling
+                }
+                
+                else if let data = try? Data.init(contentsOf: fileUrl!){
+                    //Task 1: Print the contents
+//                    if let responseString = String.init(data: data!, encoding: .ascii){
+//                        print(responseString)
+//                    }
+                    
+                    //Task 2: Parse the data as JSON instead
+                    
+//                    if let jsonObj = try? JSONSerialization.jsonObject(with: data!, options: .allowFragments){
+//                        if jsonObj is [Any] {
+//                            print("Array:\(jsonObj)")
+//                        }
+//                        if jsonObj is [String:Any]{
+//                            print("Dict:\(jsonObj)")
+//                        }
+//                    }
+                    
+                    //Task 3: Download the image by changing the URL
+                    
+                    let imageFromServer = UIImage.init(data: data)
+                    let imageView = UIImageView.init(frame: self.view.frame)
+                    imageView.image = imageFromServer
+                    imageView.contentMode = .scaleAspectFill
+                    DispatchQueue.main.async {             //Async thread to display in the main thread
+                        self.view.addSubview(imageView)
+                    }
+                    
+                }
+            })
+            task.resume()
+        }
+        
+    }
 }
 
